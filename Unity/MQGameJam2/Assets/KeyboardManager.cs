413e7a5a04c519e05edab1b66051cc7a69a11a37
ZeppelinGames <79881241+marksabamq@users.noreply.github.com>
2022-09-20 17:51:11 +1000

    Draggable keys

--- Unity/MQGameJam2/Assets/KeyboardManager.cs
+++ Unity/MQGameJam2/Assets/KeyboardManager.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-struct KeySocket
+
+class KeySocket
 {
     public string key;
     public Transform keyObject;
@@ -12,6 +13,8 @@ struct KeySocket
 
 public class KeyboardManager : MonoBehaviour
 {
+    [SerializeField] private Camera cam;
+
     [SerializeField] private KeySocket[] keys;
     [SerializeField] private Transform[] keyExplosionPositions;
 
@@ -22,12 +25,18 @@ public class KeyboardManager : MonoBehaviour
 
     [SerializeField] private bool explode = false;
 
+    [SerializeField] private float keySnapPos = 0.25f;
+
     private List<KeySocket> explodingKeys = new List<KeySocket>();
     private List<Transform> explodePositions = new List<Transform>();
     private List<float> tVals = new List<float>();
 
+    [SerializeField] private LayerMask keyMask;
+
     private KeyboardState keyboardState = KeyboardState.NONE;
     enum KeyboardState { NONE, EXPLODING, BROKEN, FIXED };
+
+    private Transform draggingObject;
 
     // Start is called before the first frame update
     void Start()
@@ -84,7 +93,7 @@ public class KeyboardManager : MonoBehaviour
                     explodingKeys[i].keyObject.rotation = Quaternion.Euler(tVals[i] * 270, (1 - tVals[i]) * 360, (tVals[i]) * 360);
                     tVals[i] += Time.deltaTime * explodeSpeed;
 
-                    if (tVals[i] < 1)
+                    if (tVals[i] <= 1)
                     {
                         allComplete = false;
                     }
@@ -93,10 +102,78 @@ public class KeyboardManager : MonoBehaviour
 
                 if (allComplete)
                 {
-                    keyboardState = KeyboardState.BROKEN;
+                    SetState(KeyboardState.BROKEN);
                 }
 
                 break;
+
+            case KeyboardState.BROKEN:
+                //allow dragging pieces 
+                if (Input.GetMouseButtonDown(0))
+                {
+                    RaycastHit hit;
+                    Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        Debug.Log(hit.transform.name);
+                        if (hit.transform.gameObject.layer == 6)
+                        {
+                            draggingObject = hit.transform;
+                        }
+                    }
+                }
+
+                if (draggingObject != null)
+                {
+                    RaycastHit hit;
+                    Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+                    if (Physics.Raycast(ray, out hit, 100, keyMask))
+                    {
+                        Debug.Log(hit.transform.name);
+                        Vector3 movePos = hit.point - (ray.direction * 0.1f);
+                        draggingObject.position = movePos;
+                    }
+                }
+
+                if (Input.GetMouseButtonUp(0))
+                {
+                    //see if dragging object is close to slot
+                    KeySocket key = null;
+                    for (int i = 0; i < keys.Length; i++)
+                    {
+                        Vector3 transformed = keys[i].keySocket.InverseTransformPoint(draggingObject.position);
+                        //Debug.Log(transformed + " " + keys[i].key);
+
+                        float offset = Mathf.Abs(transformed.x) + Mathf.Abs(transformed.z);
+                        if (offset < keySnapPos)
+                        {
+                            draggingObject.transform.position = keys[i].keySocket.position;
+                        }
+
+                        if (keys[i].keyObject == draggingObject)
+                        {
+                            key = keys[i];
+                        }
+                    }
+                    if (key != null)
+                    {
+                        explodingKeys.Remove(key);
+
+                        if (explodingKeys.Count == 0)
+                        {
+                            SetState(KeyboardState.FIXED);
+                        }
+                        Debug.Log("Set in: " + key.key);
+                    }
+
+                    draggingObject = null;
+                }
+                break;
         }
+    }
+
+    void SetState(KeyboardState newState)
+    {
+        keyboardState = newState;
     }
 }
